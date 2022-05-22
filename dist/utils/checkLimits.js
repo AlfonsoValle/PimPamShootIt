4c3a216b76cb4179fabf4de9b0b5f094734245af
AlfonsoValle <valleuned@gmail.com>
2022-05-23 01:09:00 +0200

    deploy

--- dist/utils/checkLimits.js
+++
@@ -1,9 +0,0 @@
-export const checkLimits = (position) => {
-    if (position.x < 1024 &&
-        position.x > 0 &&
-        position.y > 0 &&
-        position.y < 1024) {
-        return true;
-    }
-    return false;
-};
